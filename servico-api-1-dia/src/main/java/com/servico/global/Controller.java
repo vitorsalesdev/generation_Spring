a2c4f83f402d38f1f65c93eec857e0730966ee58
Vitor Sales <vitorsalesdev@gmail.com>
2020-10-04 21:52:51 -0300

    Aula API serviços atualizada

--- servico-api-1-dia/src/main/java/com/servico/global/Controller.java
+++ servico-api-1-dia/src/main/java/com/servico/global/Controller.java
@@ -1,10 +1,10 @@
 package com.servico.global;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +22,18 @@ public class Controller {
 
 	@PostMapping("/servicos")
 	public ServicoModel criar(@RequestBody ServicoModel model) {
+		repository.save(model);
+		return model;
+	}
+	
+	@GetMapping("/servicos/nome/{nome}")
+	public List<ServicoModel> buscarPorNome(@PathVariable String nome) {
+		return repository.findByNome(nome);
+	}
+
+	@PutMapping("/servicos/{id}")
+	public ServicoModel atualizar(@PathVariable Long id, @RequestBody ServicoModel model) {
+		model.setId(id);
 		repository.save(model);
 		return model;
 	}
